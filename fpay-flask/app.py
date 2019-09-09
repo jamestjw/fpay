44d1907de5839acde57f67c295f8fed4a3166642
James Tan <james_tan97@outlook.com>
2019-09-09 18:29:26 +0800

    Added web app

---
+++ fpay-flask/app.py
@@ -0,0 +1,101 @@
+import os
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+
+from PIL import Image
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from scipy.spatial import KDTree
+
+from openface import *
+###########  HYPERPARAMETERS  ##################################
+#Skip image with more than one face
+skipMulti = True
+size=96
+landmarkIndices = AlignDlib.OUTER_EYES_AND_NOSE
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+###########  MODEL PATH & INSTANTIATION  #######################
+fileDir = '/root/openface'
+modelDir = os.path.join(fileDir, 'models')
+dlibModelDir = os.path.join(modelDir, 'dlib')
+face_predictor_path = os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat")
+model_path = os.path.join(modelDir, 'openface', 'nn4.small2.v1.t7')
+
+#alignment model
+align = AlignDlib(face_predictor_path)
+#embedding model
+model = TorchNeuralNet(model=model_path)
+#############  HELPER FUNCTIONS  ############################
+def get_embeddings(rgb, plot=False):
+    '''
+    Get embeddings for an images.
+    '''
+    outRgb = align.align(size, rgb, landmarkIndices=landmarkIndices, skipMulti=skipMulti)
+    out128 = model.forward(outRgb) 
+    if plot:
+        plt.imshow(outRgb)
+    return out128
+#############################################################
+try:
+    data_df = pd.read_csv('data.csv')
+except:
+    data_df = pd.DataFrame([['dummy_name','dummy_phone']+list(np.random.randn(128))],columns=['name','phone']+[i for i in range(128)])
+@app.route('/')
+def index():
+    return "Hello World!"
+
+@app.route('/success')
+def success():
+    return render_template("success.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            im_array = np.array(Image.open(image))[:,:,:3]
+            embedding = get_embeddings(im_array)
+            to_append = pd.DataFrame([[request.form['user_name'],request.form['phone']]+[v for v in embedding]],columns=['name','phone']+[str(i) for i in range(128)])
+            
+            try:
+                data_df = pd.read_csv('data.csv')
+            except:
+                data_df = pd.DataFrame([['dummy_name','dummy_phone']+list(np.random.randn(128))],columns=['name','phone']+[str(i) for i in range(128)])              
+            data_df = data_df.append(to_append,sort=False)
+            data_df.to_csv('data.csv',index=False)
+            return redirect(url_for('success'))
+    return render_template("upload.html")
+
+
+@app.route('/identify', methods=["GET", "POST"])
+def identify():
+    if request.method == "POST":
+        if request.files:
+            image = np.array(Image.open(request.files['image_blob']))
+            try:
+                embedding = get_embeddings(image)
+                data_df = pd.read_csv('data.csv')
+                names = data_df['name'].tolist()
+                all_embeddings = data_df.iloc[:,2:].values
+                tree = KDTree(all_embeddings)
+                dist,idx = tree.query(embedding,k=1)
+                if dist < 0.9210277973000384:
+                    name = names[idx]
+                    response = {'name':name}
+                    return jsonify(response)
+                else:
+                    return jsonify()
+            except:
+                return jsonify()
+    return render_template('identify.html')
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Undefined route: {}!".format(name)
+
+
+if __name__ == '__main__':
+    app.run(port=9000, host='0.0.0.0')
---
+++ fpay-flask/static/js/identify.js
@@ -0,0 +1,67 @@
+var video = document.querySelector("#videoElement");
+var imageCapture;
+if (navigator.mediaDevices.getUserMedia) {
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        video.srcObject = stream;
+        const track = stream.getVideoTracks()[0];
+        imageCapture = new ImageCapture(track);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+function onGrabFrameButtonClick() {
+imageCapture.grabFrame()
+.then(imageBitmap => {
+    const canvas = document.querySelector('#grabFrameCanvas');
+    drawCanvas(canvas, imageBitmap);
+})
+.catch(error => console.log(error));
+}
+
+function onTakePhotoButtonClick() {
+imageCapture.takePhoto()
+.then(blob => {
+    var form = new FormData();
+    form.append("image_blob", blob, 'lolblob');
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/identify');
+    xhr.onreadystatechange = function() { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            response = JSON.parse(xhr.response)
+            name = response['name']
+          if (name ==undefined || name ==='undefined'){
+                document.querySelector('#output').innerText = 'Unable to find match. Kindly register with us before trying again.'
+            } else {
+                document.querySelector('#output').innerText = name
+            }
+        }
+    }
+    xhr.send(form)
+})
+.catch(error => console.log(error));
+}
+
+function drawCanvas(canvas, img) {
+canvas.width = getComputedStyle(canvas).width.split('px')[0];
+canvas.height = getComputedStyle(canvas).height.split('px')[0];
+let ratio  = Math.min(canvas.width / img.width, canvas.height / img.height);
+let x = (canvas.width - img.width * ratio) / 2;
+let y = (canvas.height - img.height * ratio) / 2;
+canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,
+    x, y, img.width * ratio, img.height * ratio);
+}
+
+function stop(e) {
+var stream = video.srcObject;
+var tracks = stream.getTracks();
+
+for (var i = 0; i < tracks.length; i++) {
+    var track = tracks[i];
+    track.stop();
+}
+video.srcObject = null;
+}
